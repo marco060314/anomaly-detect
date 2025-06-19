742d8f4c957e5ff485f37b491b6b962b748685d0
Marco Wang <marco060314@gmail.com>
2025-06-19 11:15:54 -0700

    org and analysis

---
+++ organization.py
@@ -0,0 +1,36 @@
+import pandas as pandas
+import os
+
+class organize_data:
+    def __init__(self):
+        self.df = None
+
+        #change once upload file has been setup
+        def load_file(self, filepath):
+            ext = os.path.splittext(filepath)[1].lower()
+
+        if ext == ".csv":
+            self.df = pd.read_csv(filepath)
+        elif ext in [".xls", ".xlsx"]:
+            self.df = pd.read_excel(filepath)
+        elif ext == ".json":
+            self.df = pd.read_json(filepath)
+        else:
+            raise ValueError(f"Unsupported file type: {ext}")
+        
+        print(f"Loaded data with shape {self.df.shape}")
+        return self.df
+
+    def clean_data(self):
+        if self.df is None:
+            raise ValueError("No data loaded") 
+        self.df.dropna(how="all", inplace=True)
+        self.df.dropna(axis=1, how="all", inplace=True)
+        self.df.reset_index(drop=True, inplace=True)
+        print("removed empty rows/cols, reset index")
+        return self.df
+    
+    def get_df(self):
+        return self.df
+
+
